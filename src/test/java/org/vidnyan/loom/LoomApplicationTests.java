b59a3412aa06ecfc52f73cb33b7406adc08ad2d0
Vikas Nagare <vikasnagare05@gmail.com>
2024-02-06 09:56:10 +0530

    project to explore java loom features

---
+++ src/test/java/org/vidnyan/loom/LoomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vidnyan.loom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
