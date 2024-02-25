152b9823eebb3927b6d64a488ea6ccad48ec0a37
Vikas Nagare <vikasnagare05@gmail.com>
2024-02-25 16:42:08 +0530

    added time and thread count url

--- src/main/java/org/vidnyan/loom/LoomApplication.java
+++ src/main/java/org/vidnyan/loom/LoomApplication.java
@@ -1,13 +1,15 @@
 package org.vidnyan.loom;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class LoomApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoomApplication.class, args);
+		log.info("Loom project started");
 	}
-
 }
--- src/main/java/org/vidnyan/loom/controller/PlatformController.java
+++ src/main/java/org/vidnyan/loom/controller/PlatformController.java
@@ -4,15 +4,47 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
+
 @Log4j2
 @RestController
 public class PlatformController {
-
-    @GetMapping("/platform/{value}")
-    public String platformThread(@PathVariable String value) throws InterruptedException {
-
-       log.info("Current Thread : {},  Request Number: {} " , Thread.currentThread(), value);
-       Thread.sleep(100000);
-        return "";
+    HashSet threadNames = new HashSet<>();
+    long time;
+    @GetMapping("/process")
+    public void platformThread() throws InterruptedException {
+        final var start = Instant.now();
+       log.info("Current Thread : {} " , Thread.currentThread());
+        TimeUnit.MILLISECONDS.sleep(100L);
+       // threadNames.add(Thread.currentThread().toString().substring(Thread.currentThread().toString().indexOf("worker")));
+        time = time + Duration.between(start, Instant.now()).toMillis();
     }
+
+    @GetMapping("/threadCount")
+    public long getNumberOfThreads() {
+        threadNames.forEach(o -> log.info("Thread Name : {}", o));
+        return threadNames.size();
+    }
+
+    @GetMapping("/time")
+    public long getTimeInMili() {
+        return time;
+    }
+
+    @GetMapping("/resetTime")
+    public void resetTime() {
+        time = 0L;
+    }
+    @GetMapping("/resetThreadCount")
+    public void restThreadCount() {
+        threadNames = new HashSet();
+    }
+
+
+
+
 }
